925a595b23675cc7e89a6da1eb6a348f61aeba9b
ilevin350 <ilevin350@hotmail.com>
2023-07-11 15:01:41 -0700

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -18,14 +18,13 @@ export class App {
     this.msg1 = '';
     this.msg2 = '';
     this.msg3 = '';
-    
-    // -----------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------
     // This example illustrates the unsightly result of nesting asynchronous
-    // functions vs. the preferred method of chaining promises.  While this
-    // code might not look all that bad, in a real-life scenario, the code
-    // contained in the callback functions can be complex, making the
-    // result difficult to follow.
-    // -----------------------------------------------------------------------
+    // functions vs. the preferred method of chaining promises.  While this 
+    // might not look all that bad, in a real-life scenario, the callback 
+    // functions can be complex, making the resulting code difficult to follow.
+    // --------------------------------------------------------------------------
     setTimeout(() => {
       this.msg1 = 'Async function #1 completed';
       setTimeout(() => {
